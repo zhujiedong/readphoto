a3d80135ccbc1e505529fc9e2e3397390d8ba296
zhujiedong <zhujiedong@yeah.net>
2018-11-28 20:19:52 +0800

    add 6400

--- R/read_6400.R
+++ R/read_6400.R
@@ -1,24 +1,24 @@
-#' Reads files from the LI-6800
+#' Reads files from the LI-6400
 #'
-#' \code{read_6800} Reads LI-6800 raw data files, which are delimited by tabs.
+#' \code{read_6400} Reads LI-6400 raw data files, which are delimited by tabs.
 #'
 #' @param filename path and name of your data.
-#' @param skiplines the start of your data(headline)
-#' @return read_6800 imports a LI-6800 raw data file as a data frame
+#' @param data_start the start of your data(without headline)
+#' @return read_6400 imports a LI-6400 raw data file as a data frame
 #' @importFrom utils read.delim
 #' @importFrom stringr str_split
 #' @export
 #'
 
-read_6800 <- function(filename, skiplines = 53){
-
-  data <- read.delim(filename, sep = "\t", skip = skiplines+2,
+read_6400 <- function(filename, data_start = 27){
+  
+  data <- read.delim(filename, sep = "\t", skip = data_start - 1,
                      header = FALSE, fill = TRUE, stringsAsFactors = FALSE)
   # split with the / of directory path of filename
   rm_name <- str_split(filename, "\\/")
   # only use the last part of the split list, ie data file name
   data$files <- rep(tail(rm_name[[1]], 1), nrow(data))
-
+  
   # Assign column names to data  -----------------------------
   return(data)
 }
--- R/read_bat_6400.R
+++ R/read_bat_6400.R
@@ -1,30 +1,33 @@
-#' read all raw data in one file measured by LI-6800
+#' read all raw data in one file measured by LI-6400
 #' @description help to read all raw data files with a command.
 #'
 #' @param file_dir is the file directory only contains all the measured
 #' raw data.
-#' @param skiplines the start of your data(headline)
+#' @param data_start the start of your data(without headline)
+#' @param header_line ther start of your data with header.
 #'
 #' @examples
 #' \dontrun{
 #' library(readphoto)
-#' read_bat_6800s('./6800')
+#' read_bat_6400s('./6400')
 #' }
 #'
 #' @export
 #' 
 
-read_bat_6800 <- function(file_dir, skiplines = 53){
+read_bat_6400 <- function(file_dir, header_line = 17, data_start = 27){
+
 # use the first file's header as the colnames -----------------------------
   
   file_names <-  list.files(file_dir, full.names = TRUE)
-  data_name <- read.delim(file_names[[1]], sep = "\t", skip = skiplines)
+  data_name <- read.delim(file_names[[1]], sep = "\t", skip = header_line - 1)
 
 # apply lyapply to cycle all the files ------------------------------------
   
-  read_6800 <- match.fun("read_6800")
-  data_list <- lapply(file_names, read_6800, skiplines = skiplines)
+  read_6400 <- match.fun("read_6400")
+  data_list <- lapply(file_names, read_6400, data_start = data_start)
   df <- do.call("rbind", data_list)
   colnames(df) <- colnames(data_name)
+  df <- df[which(df$FTime>0), ]
   return(df)
 }
